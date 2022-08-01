6b1efabeb8b795b5e115c5ae873d8917d6c88dc4
David Torres <davidtorresim@gmail.com>
2022-07-31 21:04:33 -0400

    Persistencia de localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -2,20 +2,52 @@ import React from 'react';
 import './App.css';
 import { AppUI } from './AppUI';
 
-const defaultTodos = [  /// datos que serán enviados mediante props
+/* const defaultTodos = [  /// datos que serán enviados mediante props
   {text: 'Ordenar', completed: false},
   {text: 'Barrer', completed: true},
   {text: 'Trapear', completed: false},
   {text: 'Trotar', completed: false},
-]
+] */
+
+function useLocalStorage (itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+  
+  if (!localStorageItem){
+    localStorage.setItem(itemName,JSON.stringify(initialValue))
+    parsedItem = [];
+  } else {
+    parsedItem = JSON.parse(localStorageItem)
+  }
+  
+  const [item, setItem] = React.useState(parsedItem)
+
+  const saveItem = (newItem) => { // Recibe el NewTodos
+    const stringifiedItem = JSON.stringify(newItem); // se hace el strigify para enviar a localStorage
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem); // Se envia al estado
+  };
+
+  return [
+    item,
+    saveItem,
+  ]
+}
+
+
 
 function App() { 
-  const [todos, setTodos] = React.useState(defaultTodos)
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1',[]);
+
   const [searchValue, setSearchValue] = React.useState(''); // en este caso seachValue esta vacio, ya que el input no lleva ningun dato hasta que alguien escribe.
+
+
   const completedTodos = todos.filter(todo => !!todo.completed).length // ! = falso, !! = verdadero
   const totalTodos = todos.length
 
   let searchedTodos = []; // Array vacio que utilizaremos mas adelante
+
+
 
   if (!searchValue.length >= 1){ 
     searchedTodos = todos; // si searchTodos esta vacio, entonces muestra todos
@@ -26,19 +58,22 @@ function App() {
       return todoText.includes(searchText); // devuelve el texto que incluye las palabras escritas en el input
     })
   }
+  
+
+
 
   const completeTodo = (text) => { // se le envía el text como props
     const todoIndex = todos.findIndex(todo => todo.text === text) // se busca el text en cada todo, una vez que lo encuentra devuelve su posicion
     const newTodos = [...todos];
-    newTodos[todoIndex].completed = true;
-    setTodos(newTodos)
+    newTodos[todoIndex].completed = true; // Se actualiza el newTodos
+    saveTodos(newTodos) // Se envía a saveTodos
   }
 
   const deleteTodo = (text) => { // se le envía el text como props
     const todoIndex = todos.findIndex(todo => todo.text === text) // se busca el text en cada todo, una vez que lo encuentra devuelve su posicion
     const newTodos = [...todos];
-    newTodos.splice(todoIndex, 1);
-    setTodos(newTodos)
+    newTodos.splice(todoIndex, 1); // Se actualiza el newTodos
+    saveTodos(newTodos) // Se envía a saveTodos
   }
 
 
